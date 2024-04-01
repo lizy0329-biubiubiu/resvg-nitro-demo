bffc102d5a68ce6cea94089aa1a8feaef019f29e
lizhiyuan <lizy@mama.cn>
2024-04-01 12:13:15 +0800

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # resvg-nitro-demo
-svg to image demo use resvg.js with nitro.js
+
+svg to image demo use [resvg.js](https://github.com/yisibl/resvg-js) with [nitro.js](https://nitro.unjs.io/guide#quick-start)
+
+## How to start
+
+```
+git clone git@github.com:lizy0329-biubiubiu/resvg-nitro-demo.git
+cd resvg-nitro-demo
+pnpm i
+pnpm dev
+```
+
+## Send file
+
+1. download the [bruno](https://github.com/usebruno/bruno) client
+2. set a post request(http://localhost:3000/api/genImageFormSvg) in bruno-client
+3. upload text.svg in body tab
+4. send!
+
+the image is gen in your rootDir.
+
---
+++ server/api/genImageFormSvg.post.ts
@@ -0,0 +1,48 @@
+import { promises } from 'node:fs';
+import { join, resolve } from 'node:path';
+import { Resvg } from '@resvg/resvg-js';
+
+const __dirname = resolve();
+
+export default eventHandler(async (event) => {
+    const formData = await readMultipartFormData(event);
+
+    formData.forEach((item) => {
+        if (item.type === 'image/svg+xml') {
+            const resvg = new Resvg(item.data, {
+                fitTo: {
+                    mode: 'width',
+                    value: 1000,
+                },
+                font: {
+                    fontFiles: [
+                        join(__dirname, 'server/public/shijuef-Regular.ttf'),
+                    ], // Load custom fonts.
+                    loadSystemFonts: false, // It will be faster to disable loading system fonts.
+                    defaultFontFamily: 'shijuef.com(gongfanmianfeiti)', // You can omit this.
+                },
+                logLevel: 'debug',
+            });
+
+            const pngData = resvg.render();
+            const pngBuffer = pngData.asPng();
+
+            console.info(
+                'Original SVG Size:',
+                `${resvg.width} x ${resvg.height}`
+            );
+            console.info(
+                'Output PNG Size  :',
+                `${pngData.width} x ${pngData.height}`
+            );
+
+            // 写入系统
+            promises.writeFile(
+                join(__dirname, `./${item.name}-out.png`),
+                pngBuffer
+            );
+        }
+    });
+
+    return { message: '⚡️ Tadaa!', images: ['img/pig.png'] };
+});
